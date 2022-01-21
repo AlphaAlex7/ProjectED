9e42864819d718ac63a0449960ed68c5dc1d7f4e
Alex Agarkov <alphaalex7@yandex.ru>
2022-01-21 16:08:04 +0300

    optimization query

--- init_data_creator.py
+++ init_data_creator.py
@@ -4,13 +4,13 @@ import string
 import types
 import django
 from django.db.transaction import atomic
-from django.db import connection
+from django.db import connection, reset_queries
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectED.settings')
 django.setup()
 
 from django.db.models import Q
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import User, Group, Permission, make_password
 from blog.models import PostModel, PostComment, PostCategory
 
 name_list = ["Zharya", "Abraham", "Abram", "Oz", "Michael"]
@@ -18,26 +18,32 @@ category_list = ["Категория 1", "Еще один", "Тест.Кат", "
 
 
 def create_user_group():
-    perm = Permission.objects.filter(Q(name__icontains="Комментарий") | Q(name__icontains="Статья"))
-    user_group = Group(name="user")
-    user_group.save()
-    user_group.permissions.set(perm)
+    group, flag = Group.objects.get_or_create(name="user")
+    if flag:
+        perm = Permission.objects.filter(Q(name__icontains="Комментарий") | Q(name__icontains="Статья"))
+        group.permissions.set(perm)
 
 
-def del_test_users():
+def del_all():
+    PostModel.objects.all().delete()
+    PostCategory.objects.all().delete()
     User.objects.filter(username__in=name_list).delete()
+    reset_queries()
 
 
 def create_test_user():
-    user_group = Group.objects.get(name="user")
-
-    users = [
-        User.objects.create_user(
+    User.objects.bulk_create([
+        User(
             username=name,
             email=f"{name}@pidr.net",
-            password="123456789").groups.set((user_group,))
-        for name in name_list]
-    return users
+            password=make_password("123456"))
+        for name in name_list])
+
+
+def add_user_to_group():
+    Group.objects.get(name="user").user_set.set(
+        User.objects.filter(username__in=name_list)
+    )
 
 
 def create_category():
@@ -65,25 +71,31 @@ def create_posts():
         for key, value in posts.items()
     ])
 
+
 def create_comment():
     post_id = [i["pk"] for i in PostModel.objects.all().values("pk")]
     author = [i["pk"] for i in User.objects.filter(is_superuser=False).values("pk")]
 
     PostComment.objects.bulk_create([
         PostComment(
-            text="".join([random.choice(list(string.ascii_letters)+[" ",]*10) for _ in range(random.randint(10,100))]),
+            text="".join(
+                [random.choice(list(string.ascii_letters) + [" ", ] * 10) for _ in range(random.randint(10, 100))]),
             author_id=random.choice(author),
             post_id=random.choice(post_id)
         ) for _ in range(100)
     ])
 
-if __name__ == '__main__':
+
+def main():
+    del_all()
     create_user_group()
-    # del_test_users()
     create_test_user()
+    add_user_to_group()
     create_category()
     create_posts()
     create_comment()
+
+
+if __name__ == '__main__':
+    main()
     print(*connection.queries, sep="\n")
-    print(sum([float(i["time"]) for i in connection.queries]))
-    pass
